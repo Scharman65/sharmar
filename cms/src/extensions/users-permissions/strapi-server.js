185a49f8782defc7ddcbe81b0ba5a50edd045f17
Ruslan Sharmanov <dobro5691@gmail.com>
2026-07-22 15:19:17 +0200

    Extend users permissions model without replacement

---
+++ cms/src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * Extend the standard Strapi Users & Permissions user model without replacing
+ * its built-in attributes or relations.
+ */
+module.exports = (plugin) => {
+  const userContentType = plugin?.contentTypes?.user;
+  const attributes = userContentType?.schema?.attributes;
+
+  if (!attributes) {
+    throw new Error(
+      "users-permissions user schema is unavailable during plugin extension"
+    );
+  }
+
+  userContentType.schema.attributes = {
+    ...attributes,
+    must_change_password: {
+      type: "boolean",
+      default: false,
+      private: true,
+    },
+  };
+
+  return plugin;
+};
